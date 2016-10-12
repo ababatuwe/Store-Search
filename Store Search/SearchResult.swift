3b24bd4f249505f7c6c59b31bf08246aa837e599
Agaba Nkuuhe <agabankuuhe@gmail.com>
2016-10-12 18:59:00 -0400

    Add a search bar and table view. The search puts fake results in the table for now.

---
+++ Store Search/SearchResult.swift
@@ -0,0 +1,14 @@
+//
+//  SearchResult.swift
+//  Store Search
+//
+//  Created by N on 2016-10-12.
+//  Copyright © 2016 Agaba Nkuuhe. All rights reserved.
+//
+
+import Foundation
+
+class SearchResult {
+    var name = ""
+    var artistName = ""
+}

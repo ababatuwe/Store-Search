9e2bd6a4a0838e4166fd8bcc4e9518f818d6a34c
Agaba Nkuuhe <agabankuuhe@gmail.com>
2016-10-13 12:12:41 -0400

    Style the search bar and add app icon

--- Store Search/SearchViewController.swift
+++ Store Search/SearchViewController.swift
@@ -33,6 +33,7 @@ class SearchViewController: UIViewController {
         cellNib = UINib(nibName: TableViewCellIdentifiers.nothingFoundCell, bundle: nil)
         tableView.register(cellNib, forCellReuseIdentifier: TableViewCellIdentifiers.nothingFoundCell)
         
+        searchBar.becomeFirstResponder()
     }
     
     override func didReceiveMemoryWarning() {
